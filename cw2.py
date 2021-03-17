c7933f958da772f35ecf5d78924c13ebb6f4af5f
Bijuu3010 <77341198+Bijuu3010@users.noreply.github.com>
2021-03-17 11:39:53 +0100

    Update cw2.py

--- cw2.py
+++ cw2.py
@@ -6,7 +6,7 @@
 
 #zadanie 2
 
-import sys
+# import sys
 
 # print("Podaj a")
 # a = sys.stdin.readline()
@@ -17,7 +17,7 @@ import sys
 
 #zadanie 4
 
-import math 
+# import math 
 
 #x = int(input("Podaj liczbe \n"))
 
@@ -29,25 +29,90 @@ import math
 
 #zadanie 5
 
-a = int(input("Podaj liczbe \n"))
-b = int(input("Podaj liczbe \n"))
-c = int(input("Podaj liczbe \n"))
+# a = int(input("Podaj liczbe \n"))
+# b = int(input("Podaj liczbe \n"))
+# c = int(input("Podaj liczbe \n"))
 
-if 0 <= a and a <= 10:
-    print("miesci sie w przedziale")
+# if 0 <= a and a <= 10:
+#     print("miesci sie w przedziale")
     
-    if a > b or b > c:
-        print(" warunek a>b b>c spełniony 5")
+#     if a > b or b > c:
+#         print(" warunek a>b b>c spełniony 5")
     
-    else:
-        print("warunek a>b b>c nie spełniony ")
+#     else:
+#         print("warunek a>b b>c nie spełniony ")
 
-else:
-    print("nie miesci sie w przediale")
+# else:
+#     print("nie miesci sie w przediale")
 
-     
+#zadanie6
+
+# a = int(input("Liczba podzielna przez \n"))
+# b = int(input("Z jakiego zakresu liczb \n"))
+
+# for i in range(b+1):
+#     if i % a == 0:
+#         print(i)
+
+#zadanie 7
+
+# a = int(input("Ile razy ma pobrac liczbe \n"))
+
+# for i in range(a+1):
+#     b = int(input("Podaj liczbe"))
+#     print(b*b)
+
+#zadaie 8
+
+# lista = []
+
+# liczba = int(input("Ile liczb dodac do listy"))
+# licznik = 0
+# while (licznik != liczba):
+#     a = int(input("podaj liczbe \n "))
+#     lista.append(a)
+#     licznik += 1
+# print(lista)
+
+#zadanie 9
+
+# liczba = str(input("podaj ciag liczb \n"))
+# licznik = 0
+# suma = 0
+# index = 0
+# while licznik != len(liczba):
+#     cyfra = int(liczba[index])
+#     index += 1
+#     licznik += 1
+#     suma = suma + cyfra
+# print(suma)
+
+#zadania 10
+
+# a = int(input("podaj wysokos wieży: "))
+# znak = 'A' 
+# if a >=0 and a <= 10:
+#     for i in range(a+1):
+#         znak = 'A' * i
+#         print(znak)
+# else:
+#     print("nie miesci sie w przedzialle 0-10")
+
+#zadanie 11
 
 
 
 
+#zadanie 12
 
+# i = 1
+# j = 1
+# while i < 11:
+#     j = 1
+#     while j < 11:
+#         print(i, '*', j, '=', i * j)
+#         j += 1
+#     i += 1
+# for i in range (1, 11):
+#     for j in range(1, 11):
+#         print(i, '*', j, '=', i * j)
