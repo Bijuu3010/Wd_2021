be4c21b4f85ff4495e3028993739e4afe5724ee1
Bijuu3010 <77341198+Bijuu3010@users.noreply.github.com>
2021-03-17 15:40:34 +0100

    cw3

---
+++ cw3.py
@@ -0,0 +1,33 @@
+#cw3 zadanie 1
+
+# A = [1 / x for x in range(1,11)]
+# B = [2 ** x for x in range(11)]
+# C = [x for x in B if x % 4 == 0]
+# print(A)
+# print(B)
+# print(C)
+
+#cw3 zadanie 2 
+# import random as rand
+
+# matrix = [[rand.randint(0,9) for i in range(4)] for j in range(4)]
+# przekatna = [matrix[i][j] for i in range(4) for j in range(4) if i == j]
+# for x in range(4):
+#     index = x
+#     print(matrix[index])
+# print(przekatna)
+
+#cw3 zadanie 3
+
+# slownik =  {'chipsy' : 'paczka', 'owoce' : 'kg', 'napój' : 'opakowanie'}
+# odwrocone = {value: key for key, value in slownik.items()}
+# print(odwrocone)
+
+
+
+
+
+
+
+
+
