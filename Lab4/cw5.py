7c21ec693d83df2621075f76e63212b4540a5de7
Bijuu3010 <77341198+Bijuu3010@users.noreply.github.com>
2021-03-26 14:00:42 +0100

    Add files via upload

---
+++ Lab4/cw5.py
@@ -0,0 +1,34 @@
+class CiagArytmetyczny:
+
+    def pobierz_parametry(self, a1, r, n):
+        self.a1 = a1
+        self.r = r
+        self.n = n
+
+    def wyswietl_dane(self):
+        print(f'a1 = {self.a1}')
+        print(f'r = {self.r}')
+        print(f'n = {self.n}')
+        print(f'an = {self.an}')
+        print(f'suma = {self.suma}')   
+
+    def policz_elementy(self):   
+        self.an =  a1 + (n - 1) * r
+        return self.an
+
+    def policz_sume(self):
+        self.suma = ((a1 + self.an ) / 2) * n
+        return self.suma
+    
+    # def pobierz_elementy(self):   ####????
+    
+a1 = int(input("Podaj wartość a1 :"))
+r = int(input("Podaj różnicę r :"))
+n = int(input("Podaj n-ty element ciągu :"))
+
+ciag = CiagArytmetyczny()
+
+ciag.pobierz_parametry(a1,r,n)
+ciag.pobierz_elementy()
+ciag.policz_sume()
+ciag.wyswietl_dane()
