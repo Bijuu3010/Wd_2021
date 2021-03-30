f7e88d343d92d55c4ea7ed75ffa892c05a4a9645
Bijuu3010 <77341198+Bijuu3010@users.noreply.github.com>
2021-03-30 15:58:45 +0200

    poprawione

--- Lab4/cw1.py
+++ Lab4/cw1.py
@@ -1,10 +1,9 @@
+lista = []
 for i in range(1,50,1):
     if i % 4 == 0:
-        plik = open("zapis_cw1.py","w")
-        plik.writelines(str([i]))
-        print(i)
-        plik.close()
-
-
+        lista += [i]
+plik = open("zapis_cw1.py","w") 
+plik.writelines(str(lista))       
+plik.close()
 
 
