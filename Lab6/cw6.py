54141d14129dbc51b2b521e17251dc7a04ffe348
Bijuu3010 <77341198+Bijuu3010@users.noreply.github.com>
2021-04-21 14:37:36 +0200

    Update cw6.py

--- Lab6/cw6.py
+++ Lab6/cw6.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-txt1 = 'patryk'
-txt2 = 'robert'
-j = 0
-mat_diag = np.diag([txt1[i] for i in range(6)])
-mat_diag_1 = [txt2[j] for j in range(6 - j)]
-# mat_diag += np.diag(mat_diag_1, j)
-print(mat_diag)
+def zad_6():
+    imiona=['amir','lara','osak','tina']
+    loop=np.array([[x for x in slowa[i]] for i in range(4)])
+    return loop
+        
+print(zad_6())
